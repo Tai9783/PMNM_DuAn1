0e928316a23a68bc3adfca99cbc5dea9d9ac4f06
Võ Văn Tài <votai130504@gmail.com>
2025-12-19 11:01:05 +0700

    update code server.js

--- server.js
+++ server.js
@@ -117,4 +117,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => console.log("Server listening on port " + PORT));
+app.listen(PORT, "0.0.0.0", () => console.log("Server listening on port " + PORT));
